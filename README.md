ecf93830ab26a1aaeef7b73f200114c99407c51e
Marc CARRE <carre.marc@gmail.com>
2015-11-27 06:23:30 +0000

    Improved implementation. Replaced http/https modules with request. Improved documentation.

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
 # norigin
-A JSONP service to perform Cross-Origin requests.
+A simple JSONP proxy to perform Cross-Origin requests (a-la AnyOrigin or WhateverOrigin).
 
 **Features**:
 - Support for both HTTP and HTTPS.
 - Support for both JSON and JSONP.
-- Free, as in both "free speech" (Apache 2.0) and "free beer": no fee, no tracking whatsoever, etc.
+- Simple: 42 lines of JavaScript code.
 - Scalable: non-blocking I/O using Node.js.
+- Free, as in both "free speech" (Apache 2.0) and "free beer": no fee, no tracking whatsoever, etc.
 
 **Usage**:
 - CURL:
 
-        $ curl -X GET http://norigin.herokuapp.com?url=http://www.google.co.uk/
-        {"contents":"<!doctype html><html[...]</html>"}
+        $ curl -X GET http://norigin.herokuapp.com?url=https://httpbin.org/html
+        "<!DOCTYPE html>\n<html>[...]</html>"
+
+        $ curl -X GET 'http://norigin.herokuapp.com/?url=https://httpbin.org/html&callback=YourCallback'
+        /**/ typeof YourCallback === 'function' && YourCallback("<!DOCTYPE html>\n<html>[...]</html>");
 
 - jQuery:
 
         $.getJSON('http://norigin.herokuapp.com?url='+url+'&callback=?', function(data) {
-          console.log(data.contents);
+          console.log(data);
         });
 
 **Set up**:
 
 1. Install Node.js.
 2. Install all dependencies by runnning: `npm install`.
-3. Start the server by running: `node index.js`.
-4. Send requests to: [http://127.0.0.1:1337](http://127.0.0.1:1337)
+3. Start the server by running: `npm start`.
+4. Send HTTP GET requests to: [http://127.0.0.1:1337](http://127.0.0.1:1337)
--- index.js
+++ index.js
@@ -1,51 +1,41 @@
 var express = require('express');
 var url     = require('url');
 var uuid    = require('node-uuid');
-var http    = require('http');
-var https   = require('https');
+var request = require('request');
 
-http_get = function(url, id, success, error) {
-  var client = url.startsWith('https') ? https : http;
-  client.get(url, function(response) {
-    console.log('[' + id + '] Received a HTTP ' + response.statusCode + '.');
-    var count = 0;
-    var data = '';
-    response.on('data', function(chunk) {
-      count += 1;
-      data += chunk;
-    });
-    response.on('end', function() {
-      console.log('[' + id + '] Received ' + data.length + ' bytes in ' + count + ' chunks.');
-      success(data);
-    });
-  }).on('error', error);
+send = function(id, callback, resp, statusCode, body) {
+  (callback === undefined) 
+    ? resp.status(statusCode).json(body) 
+    : resp.status(statusCode).jsonp(body);
+  (statusCode === 200)
+    ? console.log('[' + id + '] Response: ' + statusCode + ' - Content-Type: ' + resp.get('Content-Type') + ' - ' + body.length + ' bytes.')
+    : console.log('[' + id + '] Response: ' + statusCode + ' - ' + body);
 };
 
 var app = express();
-app.get('/', function(request, response) {
-  var id = uuid.v1();
-  var query = url.parse(request.url, true).query;
-  console.log('[' + id + '] ' + request.ip + ' requested ' + query.url);
-  if (query.url === undefined) {
-    var message = 'Please provide a valid URL.';
-    response.status(400).type('application/json').send(message);
-    console.log('[' + id + '] Replied with a HTTP 400: ' + message);
-  } else {
-    http_get(query.url, id, function(data) {
-      if (query.callback !== undefined) {
-        response.status(200).jsonp({'contents': data});
-      } else {
-        response.status(200).json({'contents': data});
-      }
-      console.log('[' + id + '] Replied with a HTTP 200 of "Content-Type": ' + response.get('Content-Type') + '.');
-    }, function(e) {
-      response.status(500).type('application/json').send(e.message);
-      console.log('[' + id + '] Replied with a HTTP 500: ' + e.message);
-    });
-  }
+app.get('/', function(req, resp) {
+  var id = req.ip + '/' + uuid.v1().substring(0, 8);
+  var query = url.parse(req.url, true).query;
+  console.log('[' + id + '] Request: ' + JSON.stringify(query));
+
+  (query.url === undefined)
+    ? send(id, query.callback, resp, 400, 'Please provide a valid URL.')
+    : request.get(query.url)
+        .on('response', function(response) {
+          var buffer = '';
+          response.on('data', function(chunk) {
+            buffer += chunk;
+          });
+          response.on('end', function() {
+            send(id, query.callback, resp, response.statusCode, buffer);
+          });
+        })
+        .on('error', function(error) {
+          send(id, query.callback, resp, 500, error);
+        });
 });
 
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
-  console.log("Listening on port " + port);
+  console.log('Listening on port ' + port);
 });
